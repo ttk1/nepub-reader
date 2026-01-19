98fb46e3e5c4944d2c60b7dc45a11fd22db98e6b
tama <tama@ttk1.net>
2026-01-19 22:27:36 +0900

    フォーマット修正

--- bibi/extensions/custom-font.js
+++ bibi/extensions/custom-font.js
@@ -7,7 +7,7 @@ Bibi.x({
     description: "Apply custom font-family to EPUB content.",
     author: "Custom",
     version: "1.1.0"
-})(function() {
+})(function () {
     var darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
     var processedItems = [];
 
@@ -15,7 +15,7 @@ Bibi.x({
     var darkModeCSS = ' img, svg, video, picture, canvas, svg image { filter: invert(1) hue-rotate(180deg) !important; }';
 
     function updateDarkModeStyles(isDark) {
-        processedItems.forEach(function(item) {
+        processedItems.forEach(function (item) {
             if (item.styleElement && item.contentDocument) {
                 try {
                     var darkStyle = item.contentDocument.getElementById('custom-font-dark-mode');
@@ -40,11 +40,11 @@ Bibi.x({
     }
 
     // テーマ変更を監視
-    darkModeQuery.addEventListener('change', function(e) {
+    darkModeQuery.addEventListener('change', function (e) {
         updateDarkModeStyles(e.matches);
     });
 
-    E.bind("bibi:postprocessed-item", function(Item) {
+    E.bind("bibi:postprocessed-item", function (Item) {
         if (Item && Item.contentDocument) {
             var style = Item.contentDocument.createElement("style");
             style.id = 'custom-font-base';
